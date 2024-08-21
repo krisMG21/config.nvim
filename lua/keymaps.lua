2e7a7b7a084bc3427f8ca19cd1e5e39c391bb880
krisMG21 <cristimg9@gmail.com>
2024-08-21 22:25:09 +0200

    Spliting big init.lua, organizing

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,36 @@
-print 'Custom keymaps loaded'
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
 
 vim.g.maplocalleader = ' '
 
@@ -8,10 +40,4 @@ vim.keymap.set('', '<C-s>', ':w<CR>')
 vim.keymap.set('', ',r', ':CellularAutomaton make_it_rain<CR>')
 vim.keymap.set('', ',g', ':CellularAutomaton game_of_life<CR>')
 
--- vim.keymap.set('n', 'spb', '<C-w><C-s>', { desc = 'Split window horizontally' })
--- vim.keymap.set('n', 'spv', '<C-w><C-v>', { desc = 'Split window vertically' }
--- vim.keymap.set('n', '<M->>', '<C-w>>', { desc = 'Increase window width' })
--- vim.keymap.set('n', '<M-<>', '<C-w><', { desc = 'Decrease window width' })
---
--- vim.keymap.set('n', '<C-s>h', '<C-w>t<C-w>K', { desc = 'Change division to horizontal' })
--- vim.keymap.set('n', '<C-s>v', '<C-w>t<C-w>H', { desc = 'Change division to vertical' })
+print 'Custom keymaps loaded'
