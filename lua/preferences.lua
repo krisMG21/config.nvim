d24c689704c86445ad6da22f1a560941492b8c67
krisMG21 <cristimg9@gmail.com>
2024-08-29 16:25:17 +0200

    trying to not show tab lines in Dashboard
    Aaand deleting repeated code

--- lua/preferences.lua
+++ lua/preferences.lua
@@ -71,10 +71,3 @@ vim.opt.scrolloff = 10
 
 -- Code colapse
 vim.cmd 'set foldmethod=indent'
-
-vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-]]
