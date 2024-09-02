95cfc49e45d8782b04bbf4554b24a92bbd769db6
krisMG21 <cristimg9@gmail.com>
2024-09-03 00:56:06 +0200

    Better formatter and lsp for python

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -1,36 +1,40 @@
 return { -- Autoformat
-'stevearc/conform.nvim',
-event = { 'BufWritePre' },
-cmd = { 'ConformInfo' },
-keys = {
-  {
-    '<leader>f',
-    function()
-      require('conform').format { async = true, lsp_fallback = true }
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_fallback = true }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true }
+      return {
+        timeout_ms = 500,
+        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      }
     end,
-    mode = '',
-    desc = '[F]ormat buffer',
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      javascript = { 'biome ' },
+      python = { 'ruff' },
+
+      -- Conform can also run multiple formatters sequentially
+      -- python = { 'isort', 'black' },
+      --
+      -- You can use 'stop_after_first' to run the first available formatter from the list
+      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+    },
   },
-},
-opts = {
-  notify_on_error = false,
-  format_on_save = function(bufnr)
-    -- Disable "format_on_save lsp_fallback" for languages that don't
-    -- have a well standardized coding style. You can add additional
-    -- languages here or re-enable it for the disabled ones.
-    local disable_filetypes = { c = true, cpp = true }
-    return {
-      timeout_ms = 500,
-      lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-    }
-  end,
-  formatters_by_ft = {
-    lua = { 'stylua' },
-    -- Conform can also run multiple formatters sequentially
-    -- python = { 'isort', 'black' },
-    --
-    -- You can use 'stop_after_first' to run the first available formatter from the list
-    -- javascript = { "prettierd", "prettier", stop_after_first = true },
-  },
-},
 }
+
